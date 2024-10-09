3026f1765448941d08c3f6a2ddcd350de6b2cabc
mahtabmardani88 <mahtab.mardani88@gmail.com>
2024-10-09 22:14:15 +0200

    solve the problem in package json and prettier and add some way to use navbar and nv to be sure everything working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
 import CreateUser from "./pages/User/CreateUser";
 import UserList from "./pages/User/UserList";
 import Navbar from "./components/Navbar";
 
 const App = () => {
+  const location = useLocation();
   return (
     <>
-      <Navbar />
+      {location.pathname === "/" && <Navbar />}
+      {location.pathname === "/nav" && <Nav />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user" element={<UserList />} />
