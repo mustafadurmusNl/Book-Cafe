bd18bc1d32d561712f3f0f86b634889a2dfa2db2
mustafa durmus <mdurmusnl@gmail.com>
2024-10-09 14:55:41 +0200

    implemented DB set up and created books collection as in ERD

--- server/src/db/connectDB.js
+++ server/src/db/connectDB.js
@@ -1,5 +1,19 @@
+// db/connectDB.js
 import mongoose from "mongoose";
+import { logError } from "../util/logging.js";
+import { logInfo } from "../util/logging.js";
 
-const connectDB = () => mongoose.connect(process.env.MONGODB_URL);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logInfo("MongoDB connected successfully"); // Use logInfo instead of console.log
+  } catch (error) {
+    logError(new Error("Failed to connect to MongoDB: " + error.message));
+    process.exit(1); // Exit the process with failure
+  }
+};
 
 export default connectDB;
