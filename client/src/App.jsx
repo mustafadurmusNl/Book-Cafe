e01d16318c9a766b98f3f8e4e6122736978f7077
Mohamad Hajjar <69475046+M-Hajjar@users.noreply.github.com>
2024-10-12 12:08:12 +0200

    added Meta.jsx component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,12 @@ import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
 import CreateUser from "./pages/User/CreateUser";
 import UserList from "./pages/User/UserList";
-import { Helmet } from "react-helmet-async";
-import favicon from "../public/assets/favicon.ico";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   const location = useLocation();
   return (
     <>
-      <Helmet>
-        <title>Book Cafe</title>
-        <meta name="description" content="Find your favorite books at Book Cafe." />
-        <link rel="icon" href={favicon}/>
-      </Helmet>
       <Nav />
       {location.pathname === "/" && <Navbar />}
       {location.pathname === "/nav" && <Nav />}
