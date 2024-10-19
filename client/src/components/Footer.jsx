685e4bb1c983e2dec23bfec1a793140e9921aac1
yousra elmaghraby <yousraelmaghraby@macbook-pro.home>
2024-10-19 11:58:39 +0200

    add landing

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css"; // Add your styles here or in a similar file
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>&copy; {new Date().getFullYear()} Book Cafee. All rights reserved.</p>
+      <p>
+        Create by : @
+        <a href="https://github.com/HackYourFuture/cohort48-project-group-B/tree/develop">
+          Cohort 48 Group B
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
