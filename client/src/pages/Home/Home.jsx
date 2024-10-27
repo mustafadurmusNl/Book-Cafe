0cf18a7c0d7f6becf589713d35eb22da57ec123f
mustafa durmus <mdurmusnl@gmail.com>
2024-10-27 12:14:03 +0100

    conditional rendering home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import TEST_ID from "./Home.testid";
 import Landing from "../../components/Landing";
 import AuthForm from "../../components/AuthForm";
-
+import { useAuth } from "../../context/AuthContext";
 const Home = () => {
+  const { user } = useAuth();
   return (
     <div data-testid={TEST_ID.container}>
       <Landing />
-      <AuthForm />
+      {!user && <AuthForm />} 
     </div>
   );
 };
