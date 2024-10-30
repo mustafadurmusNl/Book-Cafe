b82da44f1b326d99e414d457a6819368898aefb7
yousra elmaghraby <yousraelmaghraby@macbook-pro.home>
2024-10-30 21:15:06 +0100

    add

--- client/src/components/SearchBooks.jsx
+++ client/src/components/SearchBooks.jsx
@@ -34,7 +34,7 @@ const SearchBooks = () => {
     if (!searchTerm.trim()) return;
     try {
       const response = await axios.get(
-        `api/users/books/search?query=${encodeURIComponent(searchTerm)}`,
+        `api/books/search?query=${encodeURIComponent(searchTerm)}`,
       );
       setSearchResults(response.data.items || []);
     } catch (error) {
