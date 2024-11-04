ca79c72d7e4d270ca89c4fc1f7c463e436dbdd24
yousra elmaghraby <yousraelmaghraby@macbook-pro.home>
2024-11-04 23:24:03 +0100

    add

--- server/src/controllers/passport.js
+++ server/src/controllers/passport.js
@@ -11,7 +11,6 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `${process.env.BASE_URL}api/auth/google/callback`,
-      proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
